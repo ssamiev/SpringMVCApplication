9cb31a8a7f5aaaea8cbdd9d51d48fd69f0642f8b
=Suhrob <=Suhrob.Samiev@arvand.tj>
2024-05-24 10:58:37 +0500

    Create product

--- src/main/java/com/example/springmvc/controller/ProductController.java
+++ src/main/java/com/example/springmvc/controller/ProductController.java
@@ -1,14 +1,36 @@
 package com.example.springmvc.controller;
 
+import com.example.springmvc.model.Product;
+import com.example.springmvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
 
+    private ProductRepository productRepository;
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @RequestMapping(path = "/")
     public String index(){
         return "index";
+    }
+    @RequestMapping(path ="/products/add", method = RequestMethod.GET)
+    public String createProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "edit";
+    }
+    @RequestMapping(path = "/products", method = RequestMethod.POST)
+    public String saveProduct(Product product){
+        productRepository.save(product);
+        return "redirect:/";
+
     }
 
 }
